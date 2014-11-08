151785669a78da1e36f3c3bb6490e8c5ec51150e
raymondma1 <raymondxma@gmail.com>
2014-11-07 18:52:41 -0800

    finished adding drag

--- watchout.js
+++ watchout.js
@@ -5,17 +5,15 @@
 var randomize = function(n){
   var results = [];
 
-  var x, y;
   for(var i = 0; i < n; i++) {
-    var coordinates = [];
-    x = Math.floor(Math.random() * 900);
-    y = Math.floor(Math.random() * 600);
-    coordinates.push(x);
-    coordinates.push(y);
+    var coordinates = {};
+    coordinates['X'] = Math.floor(Math.random() * 900);
+    coordinates['Y'] = Math.floor(Math.random() * 600);
     results.push(coordinates);
   }
   return results;
 }
+
 var spaceCircles = randomize(30);
 var svgContainer = d3.select("body").append("svg")
  .attr("width", 1100)
@@ -28,9 +26,10 @@ var circles = svgContainer.selectAll("circle")
 
 function update(arr) {
   var spaceCircles = arr;
+
   circles.data(arr).transition().duration(1000).delay(500)
-   .attr("cx", function (d) { return d[0]; })
-   .attr("cy", function (d) { return d[1]; })
+   .attr("cx", function (d) { return d.X; })
+   .attr("cy", function (d) { return d.Y; })
    .attr("r", 10 )
 
 }
@@ -38,3 +37,43 @@ function update(arr) {
  update(randomize(30));
 
  setInterval(function(){update(randomize(30))}, 2000);
+
+ var Player = function(x, y, r) {
+  this.X = x;
+  this.Y = y;
+  this.radius = r;
+  this.highScore = 0;
+  this.curScore = 0;
+  this.collisions = 0;
+ }
+
+
+
+ Player.prototype.collision = function() {
+
+ }
+
+ Player.prototype.updateScore = function() {
+
+ }
+
+ var drag = d3.behavior.drag()
+              .on('drag', function() {d3.select(this).attr('cx', d3.event.x)
+                                             .attr('cy', d3.event.y); })
+ var player = [];
+ player.push(new Player(450,300,10));
+ var user = svgContainer.selectAll("user").data(player)
+ .enter()
+ .append("circle")
+ .attr("class", "user")
+ .call(drag);
+
+  user.data(player)
+   .attr("cx", function (d) { return d.X; })
+   .attr("cy", function (d) { return d.Y; })
+   .attr("r", 10 );
+
+
+
+
+
